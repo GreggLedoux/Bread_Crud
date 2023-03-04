f1c5ba73403b0609c5ec696aa4309d8df88968f1
GreggLedoux <guitlouie@gmail.com>
2023-03-04 12:37:36 -0500

    have been forgetting to commit this project

--- controllers/bread_controllers.js
+++ controllers/bread_controllers.js
@@ -1,17 +1,114 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/breads.js')
+const breadSeedData = require('../models/seed.js')
 
 //INDEX
 breads.get('/', (req, res) => {
-  res.send('This is the index at /breads')
+  Bread.find()
+  .then(foundBreads => {
+  res.render('This is the index at /breads')
+  res.render('Index', {
+    "breads": Bread,
+    "title": 'Index Page'
+    })
+  })
+})
+
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
+})
+
+// SHOW
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+          res.render('show', {
+              bread: foundBread
+          })
+      })
+      .catch(err => {
+        res.send('404')
+      })
+})
+
+// EDIT
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
+})
+
+// UPDATE
+breads.put('/:id', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true})
+  .then(updateBread => {
+    console.log(updatedBread)
+    res.redirect(`/breads/${req.params.id}`)
+  })
+})
+//create
+breads.post('/', (req, res) => {
+  if(!req.body.image) {
+      req.body.image = undefined 
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.create(req.body)
+  res.redirect('/breads')
+})
+
+// DELETE
+breads.delete('/:id', (req, res) => {
+  Bread.finByIdAndDelete(req.params.id)
+  .then(deletedBread => {
+    console.log(deletedBread);
+    res.status(303).redirect('/breads')
+  })
 })
 
 
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
-  
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany([
+    {
+      name: 'Rye',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'French',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'Gluten Free',
+      hasGluten: false,
+      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'Pumpernickel',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    }
+  ]
+  )
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
+
+
 
   module.exports = breads
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
 
 require('dotenv').config()
 const PORT = process.env.PORT
-console.log(PORT)
 const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
+// MIDDLEWARE
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+//routes
 app.get('/', (req, res) => {
     res.send("<h1> Awesome App About Breads</h1>")
 }) 
-// ROUTES
+
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
@@ -17,6 +31,10 @@ app.get('/', (req, res) => {
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
+  // 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 app.listen(PORT, () => {
     console.log("Listening on pert:", PORT );
